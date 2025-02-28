840598a4a137c0db174a772ca8868f09d926e0a2
ryhhill1998 <ryhhill1998@outlook.com>
2025-02-28 08:45:21 +0000

    made spotify service an abstract base class to prevent instantiation

--- api/services/spotify/spotify_service.py
+++ api/services/spotify/spotify_service.py
@@ -1,7 +1,9 @@
+from abc import ABC
+
 from api.services.endpoint_requester import EndpointRequester
 
 
-class SpotifyService:
+class SpotifyService(ABC):
     def __init__(self, client_id: str, client_secret: str, base_url: str, endpoint_requester: EndpointRequester):
         self.client_id = client_id
         self.client_secret = client_secret
